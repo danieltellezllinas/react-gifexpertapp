05fd945b7863966366f891655c1cd236f9cc9730
Daniel Tellez <danieltellezllinaas@gmail.com>
2020-08-13 18:32:24 +0200

    Firt Commit

---
+++ docs/precache-manifest.3e192d4a68a3c4f430ea511404982287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990cf2717c2f23ed8849b61f42489e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08664d46deeeae22bb7a",
+    "url": "/static/css/main.2b8aada4.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08664d46deeeae22bb7a",
+    "url": "/static/js/main.5e7a01aa.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
